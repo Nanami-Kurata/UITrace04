ab6ce6de0b55561266ae3844ec3c43e840293da1
Nanami Kurata <krtnnm20011031@gmail.com>
2021-08-17 12:46:53 +0900

    first commit

---
+++ UITrace04/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UITrace04
+//
+//  Created by 倉田七海 on 2021/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var image1: UIImageView!
+    
+}
+
